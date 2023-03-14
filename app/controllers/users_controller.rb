73b48810f879f621e5e60990d7bfc83397fde7f0
naoki316 <takehara316@me.com>
2023-03-14 21:33:15 +0000

    勤怠８章8.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
 
   def show
     @user = User.find(params[:id])
@@ -10,7 +14,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save。
+    if @user.save
       log_in @user #保存成功後、ログインします
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
@@ -22,10 +26,36 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit      
+    end
+  end
 
   private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  #beforeフィルター
+  
+  #ログイン済みのユーザーか確認します。
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
     end
+  end
+  
+  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
 end
